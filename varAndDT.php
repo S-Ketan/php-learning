4b6678612c83a05a86b59fa4358887dba1ffa631
S-Ketan <ketan10923@gmail.com>
2025-03-01 16:19:57 +0100

    Add variable examples

---
+++ varAndDT.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $var = "Hello World!";
+    echo $var
+        ?>
+    <?php
+    $name = "John";
+    $age = 25;
+    ?>
+
+    <p>My name is <?php echo $name; ?> and I'm <?php echo $age; ?> years old</p>
+</body>
+
+</html>
