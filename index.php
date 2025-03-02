8b1194cbb3e1332afe6b7aeec1bdfa48423e106a
S-Ketan <ketan10923@gmail.com>
2025-03-02 15:09:03 +0100

    Add form for user input and include CSS stylesheets

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
+
 <body>
-    <?php
-        echo "Hello World!";
-    ?>
+    <main>
+        <form action="includes/formhandler.php" method="post">
+            <label for="firstname">Firstname?</label>
+            <input type="text" name="firstname" id="firstname" placeholder="Firstname...">
+            <label for="lastname">Lastname?</label>
+            <input type="text" name="lastname" id="lastname" placeholder="Lastname...">
+            <label for="favoritepet">Fav Pet?</label>
+            <select name="favoritepet" id="favoritepet">
+                <option value="none">None</option>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="bird">Bird</option>
+            </select>
+            <button type="submit">Submit</button>
+        </form>
+    </main>
 </body>
+
 </html>
