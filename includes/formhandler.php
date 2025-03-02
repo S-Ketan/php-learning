721184a45a452f0c85252b268508584ae3a047cf
S-Ketan <ketan10923@gmail.com>
2025-03-02 15:30:24 +0100

    Add form handling logic

---
+++ includes/formhandler.php
@@ -0,0 +1,22 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+    $firstname = htmlspecialchars($_POST["firstname"]);
+    $lastname = htmlspecialchars($_POST["lastname"]);
+    $favoritepet = htmlspecialchars($_POST["favoritepet"]);
+
+if(empty($firstname) || empty($lastname) || empty($favoritepet)) {
+    header("Location: ../index.php");
+    exit() ;
+}
+
+    echo "This is the data entered by the user";
+    echo "<br>";
+    echo "Firstname: " . $firstname;
+    echo "<br>";
+    echo "Lastname: " . $lastname;
+    echo "<br>";
+    echo "Favorite Pet: " . $favoritepet;
+}else{
+    header("Location: ../index.php");
+}
